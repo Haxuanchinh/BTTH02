64469ece3f4568bca9940e04fb3256e3a466a04b
Haxuanchinh <99165737+Haxuanchinh@users.noreply.github.com>
2022-03-20 18:19:51 +0700

    up

--- BTTH06/index.js
+++ BTTH06/index.js
@@ -13,4 +13,24 @@ function dongho() {
     document.getElementById("time").innerHTML = gio +":"+ phut +":"+ giay;
     setTimeout("dongho()",1000);
 }
+function feedback() {
+    var today = new Date();
+    var gio = today.getHours();
+
+
+    var thongbao = " ";
+    if(gio<12) {
+        thongbao = "Chào buổi sáng"
+    }
+    if(gio<17) {
+        thongbao = "Chào buổi chiều"
+    }
+    if(gio<23) {
+        thongbao = "Chào buổi tối"
+    }
+    document.getElementById("thongbao").innerHTML = thongbao;
+    setTimeout("feedback()",1000);
+
+}
 dongho();
+feedback();
